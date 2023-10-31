b293b4d693339b680056cb702f5a39d8e96cffa4
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-10-31 15:13:45 +0800

    added user.js

---
+++ api/user.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { model } = require('mongoose');
+const router = express.Router();
+
+//signup
+router.post('/signup', (req, res) => {
+
+})
+
+//signin
+router.post('/signin', (req, res) => {
+
+})
+
+module.exports = router;
