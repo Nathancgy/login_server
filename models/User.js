19d03bd58099114489a253753a20615a84bf6427
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-10-31 15:18:55 +0800

    added user.js

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    dateOfBirth: Data
+})
+
+const User = mongoose.model('User', UserSchema);
