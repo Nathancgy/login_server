f5ac922fe4dbfaf9a30f5a9f00009e92e7c102d1
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-10-31 14:58:04 +0800

    added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const port = 3000;
+
+// For accepting post form data
+const bodyParser = require('express').json
+app.use(bodyParser());
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
