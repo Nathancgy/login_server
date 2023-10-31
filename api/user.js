169a559e4d0aa265bb8c2b4b1d7d66fc077864ec
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-10-31 16:26:09 +0800

    added signup

--- api/user.js
+++ api/user.js
@@ -2,9 +2,79 @@ const express = require('express');
 const { model } = require('mongoose');
 const router = express.Router();
 
-//signup
-router.post('/signup', (req, res) => {
+// Mongodb user model
+const User = require('./../models/User')
 
+// Password handler
+const bcrypt = require('bcrypt');
+
+// Signup
+router.post('/signup', (req, res) => {
+    let {name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
+
+    if(name == "" || email == "" || password == "" || dateOfBirth == ""){
+        res.json({
+            status: "FAILED",
+            message: "Empty input fields!"
+        });
+    } else if (!/^[a-zA-Z ]*$/.test(name)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid name entered"
+        })
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid email entered"
+        })
+    } else if (!new Date(dateOfBirth).getTime()){
+        res.json({
+            status: "FAILED",
+            message: "Invalid date of birth entered"
+        })
+    } else if (password.length < 8){
+        res.json({
+            status: "FAILED",
+            message: "Password is too short!"
+        })
+    } else {
+        // Checking if user already exists
+        User.find({email}).then(result => {
+            if (result.length) {
+                // A user already exist
+                res.json({
+                    status: "FAILED",
+                    message: "User with the provided email already exists"
+                })
+            } else {
+                // Try to create new user
+                
+                // Password handling
+                const saltRounds = 10;
+                bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                    const newUser = new User({
+                        
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An error occurred while hashing password!"
+                    })
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                status: "FAILED",
+                message: "An error occurred while checking for existing user!"
+            })
+        })
+    }
 })
 
 //signin
