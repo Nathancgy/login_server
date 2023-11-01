59cf774353008dff618b9405a7b7a1cea2edcd20
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-11-01 11:26:29 +0800

    updated

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // mongodb
 require('./config/db');
-
+const express = require('express');
 const app = require('express')();
 const port = process.env.PORT || 3000;
-
+const path = require('path');
 const UserRouter = require('./api/user');
 
 // For accepting post form data
@@ -15,3 +15,9 @@ app.use('/user', UserRouter)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
+
+app.use('/static', express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
