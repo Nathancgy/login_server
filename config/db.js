670876038dda009dc6a5f6c61e24b4a6f488615d
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-11-01 10:10:43 +0800

    changed env

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => {
     console.log("DB Connected");
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
   
